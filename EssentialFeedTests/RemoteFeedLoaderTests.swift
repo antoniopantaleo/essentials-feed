59c3068c393618b4f0bc8166cab07b3fc08038f5
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-08-19 15:39:57 +0200

    add test to check call count

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -25,6 +25,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLCallCount, 2)
+        XCTAssertEqual(client.requestedURL, url)
+    }
+    
     //MARK: Helpers
     
     private func makeSUT(
@@ -37,8 +48,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLCallCount = 0
         
         func get(from url: URL) {
+            self.requestedURLCallCount += 1
             self.requestedURL = url
         }
     }
