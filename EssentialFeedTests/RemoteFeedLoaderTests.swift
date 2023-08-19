3e6c6a82d104d03189d6b8286b8f607caeaa7083
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-08-19 11:42:29 +0200

    add test plan

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,12 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Antonio on 19/08/23.
+//
+
+import XCTest
+
+final class RemoteFeedLoaderTests: XCTestCase {
+    
+}
