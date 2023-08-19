4406d41b98fd613fe23267143ceafef6fb48273c
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-08-19 11:48:31 +0200

    add first test

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,6 +7,22 @@
 
 import XCTest
 
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
 final class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        let sut = RemoteFeedLoader()
+        /// We are asserting that we didn't make a URL request since that
+        /// should only happen when `.load()` is invoked
+        XCTAssertNil(client.requestedURL)
+    }
     
 }
