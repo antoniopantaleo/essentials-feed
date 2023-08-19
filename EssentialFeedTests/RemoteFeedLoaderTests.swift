41750789486783fc9288ab77ee6d010a3b3d7b6c
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-08-19 12:06:30 +0200

    make HTTPClient a protocol and use constructor injection

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,21 +8,46 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://a-url.com")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        self.requestedURL = url
+    }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        let sut = RemoteFeedLoader()
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
         /// We are asserting that we didn't make a URL request since that
         /// should only happen when `.load()` is invoked
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
     
 }
