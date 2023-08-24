cbd35a89a895f9c55936613301d5c489cd553d4c
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-08-25 00:49:30 +0200

    create factory method to simplify test

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -97,39 +97,24 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversItemsOn200HTTPResponseWithJSONItems() {
         let (sut, client) = makeSUT()
         
-        let item1 = FeedItem(
+        let item1 = makeItem(
             id: UUID(),
-            description: nil,
-            location: nil,
             imageURL: URL(string: "https://a-url.com")!
         )
-        let item1Json = [
-            "id": item1.id.uuidString,
-            "image": item1.imageURL.absoluteString
-        ]
         
-        let item2 = FeedItem(
+        let item2 = makeItem(
             id: UUID(),
             description: "a description",
             location: "a location",
             imageURL: URL(string: "https://another-url.com")!
         )
-        let item2Json = [
-            "id": item2.id.uuidString,
-            "description": item2.description,
-            "location": item2.location,
-            "image": item2.imageURL.absoluteString
-        ]
         
-        let itemsJson = [
-            "items": [item1Json, item2Json]
-        ]
-        
+        let models = [item1.model, item2.model]
         expect(
             sut,
-            toCompleteWithResult: .success([item1, item2]),
+            toCompleteWithResult: .success(models),
             when: {
-                let json = try! JSONSerialization.data(withJSONObject: itemsJson)
+                let json = makeItemsJSON([item1.json, item2.json])
                 client.complete(withStatusCode: 200, data: json)
             }
         )
@@ -143,6 +128,38 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut, client)
+    }
+    
+    // Factory Method
+    private func makeItem(
+        id: UUID,
+        description: String? = nil,
+        location: String? = nil,
+        imageURL: URL
+    ) -> (model: FeedItem, json: [String: Any]) {
+        let model = FeedItem(
+            id: id,
+            description: description,
+            location: location,
+            imageURL: imageURL
+        )
+        
+        /// We use compact map to remove optional values
+        /// Otherwise the types don't matches
+        /// [String:Any?] != [String:Any]
+        let json = [
+            "id": model.id.uuidString,
+            "description": model.description,
+            "location": model.location,
+            "image": model.imageURL.absoluteString
+        ].compactMapValues { $0 }
+        
+        return (model, json)
+    }
+    
+    private func makeItemsJSON(_ items: [[String:Any]]) -> Data {
+        let json = ["items": items]
+        return try! JSONSerialization.data(withJSONObject: json)
     }
     
     private func expect(
