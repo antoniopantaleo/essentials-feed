2bb2ad6086d02fde90a3e7f6deed917549490ac4
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-08-27 16:43:50 +0200

    add test aganist the single test server feed items

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -16,9 +16,19 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
         
         let expectation = expectation(description: "Wait for completion")
-        loader.load { result in
+        loader.load { [weak self] result in
+            guard let self = self else { return }
             if case .success(let items) = result {
-                XCTAssertEqual(items.count, 8)
+                XCTAssertEqual(items.count, 8, "Expected 8 items in the test service")
+                XCTAssertEqual(items[0], self.expectedItem(at: 0))
+                XCTAssertEqual(items[1], self.expectedItem(at: 1))
+                XCTAssertEqual(items[2], self.expectedItem(at: 2))
+                XCTAssertEqual(items[3], self.expectedItem(at: 3))
+                XCTAssertEqual(items[4], self.expectedItem(at: 4))
+                XCTAssertEqual(items[5], self.expectedItem(at: 5))
+                XCTAssertEqual(items[6], self.expectedItem(at: 6))
+                XCTAssertEqual(items[7], self.expectedItem(at: 7))
+                
             } else {
                 XCTFail("Expected success, got \(result) instead")
             }
@@ -26,6 +36,64 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
         }
         
         wait(for: [expectation], timeout: 5.0)
+    }
+    
+    // MARK: Helpers
+    
+    private func expectedItem(at index: Int) -> FeedItem {
+        FeedItem(
+            id: id(at: index),
+            description: description(at: index),
+            location: location(at: index),
+            imageURL: imageURL(at: index)
+        )
+    }
+    
+    private func id(at index: Int) -> UUID {
+        let UUIDs: [String] = [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01",
+        ]
+        return UUID(uuidString: UUIDs[index])!
+    }
+    
+    private func description(at index: Int) -> String? {
+        let descriptions: [String?] = [
+            "Description 1",
+            nil,
+            "Description 3",
+            nil,
+            "Description 5",
+            "Description 6",
+            "Description 7",
+            "Description 8",
+        ]
+        return descriptions[index]
+    }
+    
+    private func location(at index: Int) -> String? {
+        let locations: [String?] = [
+            "Location 1",
+            "Location 2",
+            nil,
+            nil,
+            "Location 5",
+            "Location 6",
+            "Location 7",
+            "Location 8",
+        ]
+        return locations[index]
+    }
+    
+    private func imageURL(at index: Int) -> URL {
+        let urlNumber = index + 1
+        return URL(string: "https://url-\(urlNumber).com")!
     }
     
 }
