e5ee3e4341d84db9cfb1d09042f487a56aa0e107
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-09-09 13:24:03 +0200

    refactor LocalFeedLoader code

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -14,6 +14,9 @@ public final class LocalFeedLoader {
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
     
+    private let calendar = Calendar(identifier: .gregorian)
+    private var maxCacheAgeInDays: Int { 7 }
+    
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
@@ -34,8 +37,7 @@ public final class LocalFeedLoader {
     
     
     private func validate(_ timestamp: Date) -> Bool {
-        let calendar = Calendar(identifier: .gregorian)
-        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else { return false }
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
         return currentDate() < maxCacheAge
     }
     
