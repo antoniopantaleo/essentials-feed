f5d77939f9f397f54590b9f6116fe75926a59f73
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-09-09 13:22:29 +0200

    delivers no images if cache is seven days old

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -20,16 +20,23 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult) -> Void) {
-        store.retrieve { result in
+        store.retrieve { [unowned self] result in
             switch result {
-                case .empty:
-                    completion(.success([]))
                 case let .failure(error):
                     completion(.failure(error))
-                case let .found(feed, _):
+                case let .found(feed, timestamp) where self.validate(timestamp):
                     completion(.success(feed.toFeedImage))
+                case .found, .empty:
+                    completion(.success([]))
             }
         }
+    }
+    
+    
+    private func validate(_ timestamp: Date) -> Bool {
+        let calendar = Calendar(identifier: .gregorian)
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else { return false }
+        return currentDate() < maxCacheAge
     }
     
     public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -60,6 +60,20 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         )
     }
     
+    func test_load_deliversNoImagesOnSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        expect(
+            sut,
+            toCompleteWith: .success([]),
+            when: {
+                store.completeRetrieval(with: feed.localFeedItems, timestamp: sevenDaysOldTimestamp)
+            }
+        )
+    }
+    
     //MARK: Helpers
     
     private func makeSUT(
