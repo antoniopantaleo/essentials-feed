191acffa8335907f22a5ec11a5284aa52cc7ee56
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-09-09 12:11:18 +0200

    moved FeedStoreSpy to its own shared file

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -195,45 +195,4 @@ final class CacheFeedUseCaseTests: XCTestCase {
         return (items, localFeedItems)
     }
     
-    /// This class help us to abstract the framework we are going to use from the tests
-    private class FeedStoreSpy: FeedStore {
-        
-        enum ReceivedMessage: Equatable {
-            case deleteCacheFeed
-            case insert(items: [LocalFeedImage], timestamp: Date)
-        }
-        
-        private var deletionCompletions: [DeletionCompletion] = []
-        private var insertionCompletions: [InsertionCompletion] = []
-        
-        private(set) var receivedMessages = [ReceivedMessage]()
-        
-        func deleteChachedFeeds(completion: @escaping DeletionCompletion) {
-            deletionCompletions.append(completion)
-            receivedMessages.append(.deleteCacheFeed)
-        }
-        
-        func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-            receivedMessages.append(.insert(items: items, timestamp: timestamp))
-            insertionCompletions.append(completion)
-        }
-        
-        func completeDeletion(with error: Error, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-        
-        func completeInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-        
-        func completeDeletionSuccessfully(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-        
-        func completeInsertionSuccessfully(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-        
-    }
-    
 }
