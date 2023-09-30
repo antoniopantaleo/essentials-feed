5312d71cba5e71c57b3632ab51499d289da8124d
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-09-30 14:48:01 +0200

    inserting to non empty cache overrides previosuly inserted values

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -132,6 +132,23 @@ final class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .failure(anyNSError))
     }
     
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        let sut = makeSUT()
+        let firstFeed = uniqueImageFeed().localFeedItems
+        let firstTimestamp = Date()
+       
+        let firstInsertionError = insert((firstFeed, firstTimestamp), to: sut)
+        XCTAssertNil(firstInsertionError, "Expected to insert cache successfully")
+        
+        let latestFeed = uniqueImageFeed().localFeedItems
+        let latestTimestamp = Date()
+        let latestInsertionError = insert((latestFeed, latestTimestamp), to: sut)
+        XCTAssertNil(latestInsertionError, "Expected to override cache successfully")
+        
+        expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+        
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
@@ -160,13 +177,17 @@ final class CodableFeedStoreTests: XCTestCase {
         try? FileManager.default.removeItem(at: testSpecificStoreURL)
     }
     
-    private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) {
+    @discardableResult
+    private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) -> Error?{
         let expectation = expectation(description: "Wait for cache insertion")
+        var receivedError: Error? = nil
         sut.insert(cache.feed, timestamp: cache.timestamp) { insertionError in
+            receivedError = insertionError
             XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: 1.0)
+        return receivedError
     }
     
     private func expect(
