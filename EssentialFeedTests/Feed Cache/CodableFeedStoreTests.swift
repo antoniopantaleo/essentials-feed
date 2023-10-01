8d5e6837848fd42b3e56e6ee2564df40e40c835b
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2023-10-01 10:33:02 +0200

    CodableFeedStore side effects run serially

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -95,6 +95,31 @@ final class CodableFeedStoreTests: XCTestCase {
         XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
     }
     
+    func test_storeSideEffects_runSerially() {
+        let sut = makeSUT()
+        var completedOperationsInOrder = [XCTestExpectation]()
+        let operation1 = expectation(description: "Operation 1")
+        sut.insert(uniqueImageFeed().localFeedItems, timestamp: Date()) { _ in
+            completedOperationsInOrder.append(operation1)
+            operation1.fulfill()
+        }
+        
+        let operation2 = expectation(description: "Operation 2")
+        sut.deleteChachedFeeds { _ in
+            completedOperationsInOrder.append(operation2)
+            operation2.fulfill()
+        }
+        
+        let operation3 = expectation(description: "Operation 3")
+        sut.insert(uniqueImageFeed().localFeedItems, timestamp: Date()) { _ in
+            completedOperationsInOrder.append(operation3)
+            operation3.fulfill()
+        }
+        
+        wait(for: [operation1, operation2, operation3], timeout: 1.0)
+        XCTAssertEqual(completedOperationsInOrder, [operation1, operation2, operation3], "Expected side-effects to run serially but operations finished in the wrong order")
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> any FeedStore {
