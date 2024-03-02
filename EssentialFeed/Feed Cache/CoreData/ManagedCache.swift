973ffd77739b657d67d302923b4f973cc864ec1e
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-02 16:53:53 +0100

    extract ManagedObject creation and mapping inside ManagedObject classes

--- EssentialFeed/Feed Cache/CoreData/ManagedCache.swift
+++ EssentialFeed/Feed Cache/CoreData/ManagedCache.swift
@@ -14,4 +14,14 @@ public class ManagedCache: NSManagedObject, Identifiable {
     
     @NSManaged public var timestamp: Date
     @NSManaged public var feed: NSOrderedSet
+    
+    public static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    
+    public var localFeed: [LocalFeedImage] {
+        return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+    }
 }
--- EssentialFeed/Feed Cache/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/CoreData/ManagedFeedImage.swift
@@ -17,4 +17,24 @@ public class ManagedFeedImage: NSManagedObject, Identifiable {
     @NSManaged public var location: String?
     @NSManaged public var url: URL
     @NSManaged public var cache: ManagedCache
+    
+    public static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localFeed.map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.url
+            return managed
+        })
+    }
+    
+    var local: LocalFeedImage {
+        LocalFeedImage(
+            id: id,
+            description: imageDescription,
+            location: location,
+            url: url
+        )
+    }
 }
