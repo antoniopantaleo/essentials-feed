5e859c73767dbf99538155616c88a2f0d4c16b0e
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-02 17:18:19 +0100

    create empty EssentialFeedCacheIintegrationTests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Antonio on 02/03/24.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
