580da50e51e39b4e41fbffe2228c3049784004f1
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-02 16:49:25 +0100

    remoed unused methods in managed models

--- EssentialFeed/Feed Cache/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/CoreData/ManagedFeedImage.swift
@@ -11,11 +11,7 @@ import CoreData
 
 @objc(ManagedFeedImage)
 public class ManagedFeedImage: NSManagedObject, Identifiable {
-    
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<ManagedFeedImage> {
-        return NSFetchRequest<ManagedFeedImage>(entityName: "ManagedFeedImage")
-    }
-    
+
     @NSManaged public var id: UUID
     @NSManaged public var imageDescription: String?
     @NSManaged public var location: String?
