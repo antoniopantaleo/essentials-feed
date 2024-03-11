59b3b5940187d25fe956343657be3b1272758ae3
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-11 23:59:59 +0100

    created FeedPresenterTests

---
+++ EssentialFeedTests/FeedPresentation/FeedPresenterTests.swift
@@ -0,0 +1,37 @@
+//
+//  FeedPresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by Antonio on 11/03/24.
+//
+
+import XCTest
+
+final class FeedPresenter {
+    init(view: Any) {
+        
+    }
+}
+
+class FeedPresenterTests: XCTestCase {
+    
+    func test_init_doesNotSendMessagesToView() {
+        let (_, view) = makeSUT()
+        XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackMemoryLeaks(view, file: file, line: line)
+        trackMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
+    
+    private class ViewSpy {
+        let messages = [Any]()
+    }
+    
+}
