6c7dbf49ba3e37a28260956d78c2856075f78d99
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-11 23:36:24 +0100

    add title do FeedViewController

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -12,6 +12,12 @@ import EssentialFeediOS
 
 final class FeedViewControllerTests: XCTestCase {
     
+    func test_feedView_hasTitle() {
+        let (sut, _) = makeSUT()
+        sut.simulateAppearance()
+        XCTAssertEqual(sut.title, "My Feed")
+    }
+    
     func test_init_doesNotLoadFeed() {
         let (_, loader) = makeSUT()
         
