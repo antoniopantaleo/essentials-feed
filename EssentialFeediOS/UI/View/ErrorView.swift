be49073a0e3994c65fcfe1401f703d38f23529aa
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-12 20:58:09 +0100

    animate error view presentation

--- EssentialFeediOS/UI/View/ErrorView.swift
+++ EssentialFeediOS/UI/View/ErrorView.swift
@@ -11,12 +11,41 @@ final class ErrorView: UIView {
     @IBOutlet private var messageLabel: UILabel!
     
     var message: String? {
-        get { messageLabel.text }
-        set { messageLabel.text = newValue }
+        get { isVisible ? messageLabel.text : nil }
+        set { setMessageAnimated(newValue) }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        message = nil
+        alpha = 0
+    }
+    
+    private var isVisible: Bool {
+        return alpha > 0
+    }
+    
+    private func setMessageAnimated(_ message: String?) {
+        if let message = message {
+            showAnimated(message)
+        } else {
+            hideMessageAnimated()
+        }
+    }
+    
+    private func showAnimated(_ message: String) {
+        messageLabel.text = message
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+    
+    private func hideMessageAnimated() {
+        UIView.animate(
+            withDuration: 0.25,
+            animations: { [weak self] in self?.alpha = 0 },
+            completion: { [weak messageLabel] completed in
+                if completed { messageLabel?.text = nil }
+            }
+        )
     }
 }
