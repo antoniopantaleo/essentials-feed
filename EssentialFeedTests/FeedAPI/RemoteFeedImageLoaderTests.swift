adf0283f344398bd76c170f5d67e3e81cc5acd0c
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-15 20:46:46 +0100

    add RemoteFeedImageLoaderTests class

---
+++ EssentialFeedTests/FeedAPI/RemoteFeedImageLoaderTests.swift
@@ -0,0 +1,12 @@
+//
+//  RemoteFeedImageLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Antonio Pantaleo on 15/03/24.
+//
+
+import XCTest
+
+final class RemoteFeedImageLoaderTests: XCTestCase {
+    
+}
