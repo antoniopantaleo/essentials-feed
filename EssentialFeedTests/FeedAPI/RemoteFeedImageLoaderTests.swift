776de3c9d24cd16e1d6c2e82898d04fda9088f52
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-15 20:50:53 +0100

    add RemoteFeedImageLoaderTests does not perform any URL request on init

--- EssentialFeedTests/FeedAPI/RemoteFeedImageLoaderTests.swift
+++ EssentialFeedTests/FeedAPI/RemoteFeedImageLoaderTests.swift
@@ -6,7 +6,54 @@
 //
 
 import XCTest
+import EssentialFeed
+
+class RemoteFeedImageLoader: FeedImageLoader {
+    
+    private let client: HTTPClient
+    
+    struct Task: FeedImageDataLoaderTask {
+        func cancel() {}
+    }
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        Task()
+    }
+    
+    
+}
 
 final class RemoteFeedImageLoaderTests: XCTestCase {
     
+    func test_init_doesNotPerformAnyURLRequest() {
+        let (_, client) = makeSUT()
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+
+    private func makeSUT(
+        url: URL = anyURL,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: RemoteFeedImageLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageLoader(client: client)
+        trackMemoryLeaks(sut, file: file, line: line)
+        trackMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
+
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+        
+        func get(
+            from url: URL,
+            completion: @escaping (EssentialFeed.HTTPClientResult
+        ) -> Void) {
+            requestedURLs.append(url)
+        }
+    }
 }
