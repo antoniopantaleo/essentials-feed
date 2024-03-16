66e4fe7a46da4f7bcf7f2d06347e5f604cb35d94
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-16 18:49:42 +0100

    add "data" field to CoreData FeedStore ManagedFeedImage model

--- EssentialFeed/Feed Cache/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/CoreData/ManagedFeedImage.swift
@@ -16,6 +16,7 @@ class ManagedFeedImage: NSManagedObject, Identifiable {
     @NSManaged var imageDescription: String?
     @NSManaged var location: String?
     @NSManaged var url: URL
+    @NSManaged var data: Data?
     @NSManaged var cache: ManagedCache
     
     static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
