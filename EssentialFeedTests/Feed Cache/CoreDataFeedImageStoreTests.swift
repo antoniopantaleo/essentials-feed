d7172092369fa4166c5adf346986bb0f97c220b9
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-16 19:06:37 +0100

    moved CoreDataFeedStore FeedImageDataStore extension to production code

--- EssentialFeedTests/Feed Cache/CoreDataFeedImageStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedImageStoreTests.swift
@@ -8,30 +8,6 @@
 import XCTest
 @testable import EssentialFeed
 
-extension CoreDataFeedStore: FeedImageDataStore {
-
-    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
-        perform { context in
-            completion(Result {  
-                let image = try ManagedFeedImage.first(with: url, in: context)
-                image?.data = data
-                try context.save()
-            })
-        }
-    }
-    
-    
-    
-    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        perform { context in
-            completion(Result {
-                return try ManagedFeedImage.first(with: url, in: context)?.data
-            })
-        }
-    }
-
-}
-
 class CoreDataFeedImageStoreTests: XCTestCase {
 
     func test_retrieveImageData_deliversNotFoundWhenEmpty() {
