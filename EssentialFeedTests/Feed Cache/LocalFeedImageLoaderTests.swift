c8488fb42cfb3eb24d4950caf07da9b977a16968
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-16 11:16:57 +0100

    add LocalFeedImageLoaderTests

---
+++ EssentialFeedTests/Feed Cache/LocalFeedImageLoaderTests.swift
@@ -0,0 +1,13 @@
+//
+//  LocalFeedImageLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Antonio Pantaleo on 16/03/24.
+//
+
+import XCTest
+import EssentialFeed
+
+final class LocalFeedImageLoaderTests: XCTestCase {
+    
+}
