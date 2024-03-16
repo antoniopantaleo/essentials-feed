3a697b8af8939c71ecd1809b0c2761def211cace
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-16 19:08:00 +0100

    add cleanUpReferencesToPersistentStores to CoreDataFeedStore

--- EssentialFeed/Feed Cache/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreData/CoreDataFeedStore.swift
@@ -62,6 +62,17 @@ public class CoreDataFeedStore: FeedStore {
         }
     }
     
+    private func cleanUpReferencesToPersistentStores() {
+        context.performAndWait {
+            let coordinator = self.container.persistentStoreCoordinator
+            try? coordinator.persistentStores.forEach(coordinator.remove)
+        }
+    }
+    
+    deinit {
+        cleanUpReferencesToPersistentStores()
+    }
+    
 }
 
 extension CoreDataFeedStore: FeedImageDataStore {
