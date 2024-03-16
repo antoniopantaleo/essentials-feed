b2edbd3a16309eb76b0b0ed3ed5ff80ab77e82d0
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-03-16 19:01:29 +0100

    refactore CoreDataFeedStore methods

--- EssentialFeed/Feed Cache/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreData/CoreDataFeedStore.swift
@@ -24,8 +24,7 @@ public class CoreDataFeedStore: FeedStore {
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 if let cache = try ManagedCache.find(in: context) {
                     completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
@@ -39,8 +38,7 @@ public class CoreDataFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
@@ -54,8 +52,7 @@ public class CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
                 completion(nil)
