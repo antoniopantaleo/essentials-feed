9a74592275468ae01e4e9e4558f564bc62070879
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-04-21 16:08:25 +0200

    moved API protocols and infrastructures to separate folders (modules)

--- EssentialFeedTests/FeedAPI/LoadImageCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/FeedAPI/LoadImageCommentsFromRemoteUseCaseTests.swift
@@ -239,41 +239,4 @@ final class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
         action()
         wait(for: [expectation], timeout: 1.0)
     }
-    
-    private class HTTPClientSpy: HTTPClient {
-        
-        private struct Task: HTTPClientTask {
-            func cancel() { }
-        }
-        
-        typealias Message = (url: URL, completion: (HTTPClientResult) -> Void)
-        private var messages = [Message]()
-        
-        var requestedURLs: [URL] {
-            messages.map { $0.url }
-        }
-        
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) -> HTTPClientTask {
-            messages.append((url, completion))
-            return Task()
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(
-            withStatusCode code: Int,
-            data: Data,
-            at index: Int = 0
-        ) {
-            let response = HTTPURLResponse(
-                url: requestedURLs[index],
-                statusCode: code,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-            messages[index].completion(.success((data, response)))
-        }
-    }
 }
