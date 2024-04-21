a6b183cde4548c7e610d6c5dbef94f02ecbe6c60
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-04-21 16:46:26 +0200

    test ImageCommentsMapper in isolation

--- EssentialFeedTests/FeedAPI/FeedItemsMapperTests.swift
+++ EssentialFeedTests/FeedAPI/FeedItemsMapperTests.swift
@@ -61,28 +61,10 @@ class FeedItemsMapperTests: XCTestCase {
         return .failure(error)
     }
 
-    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedImage, json: [String: Any]) {
-        let item = FeedImage(id: id, description: description, location: location, url: imageURL)
-
-        let json = [
-            "id": id.uuidString,
-            "description": description,
-            "location": location,
-            "image": imageURL.absoluteString
-        ].compactMapValues { $0 }
-
-        return (item, json)
-    }
-
-    private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
-        let json = ["items": items]
-        return try! JSONSerialization.data(withJSONObject: json)
-    }
-
 }
 
 private extension HTTPURLResponse {
     convenience init(statusCode: Int) {
-        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+        self.init(url: anyURL, statusCode: statusCode, httpVersion: nil, headerFields: nil)!
     }
 }
--- EssentialFeedTests/Helpers/SharedTestHelpers.swift
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import EssentialFeed
 
 var anyNSError: NSError {
     NSError(domain: "any error", code: 0)
@@ -13,4 +14,35 @@ var anyNSError: NSError {
 
 var anyURL: URL {
     URL(string: "https://any-url.com")!
+}
+
+func makeItem(
+    id: UUID,
+    description: String? = nil,
+    location: String? = nil,
+    imageURL: URL
+) -> (
+    model: FeedImage,
+    json: [String: Any]
+) {
+    let item = FeedImage(
+        id: id,
+        description: description,
+        location: location,
+        url: imageURL
+    )
+    
+    let json = [
+        "id": id.uuidString,
+        "description": description,
+        "location": location,
+        "image": imageURL.absoluteString
+    ].compactMapValues { $0 }
+    
+    return (item, json)
+}
+
+func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+    let json = ["items": items]
+    return try! JSONSerialization.data(withJSONObject: json)
 }
