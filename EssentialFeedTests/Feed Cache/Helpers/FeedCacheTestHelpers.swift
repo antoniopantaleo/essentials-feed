17034a62adc21ce1160356452d7d543d48b5dc27
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2024-05-01 20:18:56 +0200

    add mapping from ImageComments into ImageCommentsViewModels

--- EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -15,15 +15,6 @@ extension Date {
         adding(days: -feedCacheMaxAgeInDays)
     }
     
-    private func adding(days: Int) -> Date {
-        Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-}
-
-extension Date {
-    func adding(seconds: TimeInterval) -> Date {
-        self + seconds
-    }
 }
 
 func uniqueImage() -> FeedImage {
--- EssentialFeedTests/Image Comments Presentation/ImageCommentsPresentationTests.swift
+++ EssentialFeedTests/Image Comments Presentation/ImageCommentsPresentationTests.swift
@@ -14,6 +14,38 @@ final class ImageCommentsPresenterTests: XCTestCase {
         XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
     }
     
+    func test_map_createsViewModels() {
+        let now = Date()
+        
+        let comments = [
+            ImageComment(
+                id: UUID(),
+                message: "a message",
+                createdAt: now.adding(minutes: -5),
+                username: "a username"),
+            ImageComment(
+                id: UUID(),
+                message: "another message",
+                createdAt: now.adding(days: -1),
+                username: "another username")
+        ]
+        
+        let viewModel = ImageCommentsPresenter.map(comments)
+        
+        XCTAssertEqual(viewModel.comments, [
+            ImageCommentViewModel(
+                message: "a message",
+                date: "5 minuti fa",
+                username: "a username"
+            ),
+            ImageCommentViewModel(
+                message: "another message",
+                date: "1 giorno fa",
+                username: "another username"
+            )
+        ])
+    }
+    
     // MARK: - Helpers
     
     private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
